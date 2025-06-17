6c06ed1d33a087d9c4c676c03872232126f61e35
Damar <dsmora2411carranza@gmail.com>
2025-06-17 09:14:12 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -8,25 +8,41 @@ if (hora < 12) {
   saludo.textContent = "¡Buenas tardes, gracias por visitar!";
 } else {
   saludo.textContent = "¡Buenas noches, espero que estés bien!";
+}
+
+// Función para mostrar mensaje con animación
 function mostrarMensaje() {
   saludo.classList.add('fade-out');
-
   setTimeout(() => {
     alert("Rico tonto🥵🥵");
     saludo.classList.remove('fade-out');
   }, 500);
 }
 
+// Mostrar la fecha actual
 const fechaP = document.getElementById('fecha');
 const opcionesFecha = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const fechaActual = new Date().toLocaleDateString('es-ES', opcionesFecha);
-
 fechaP.textContent = `Hoy es ${fechaActual}`;
-
-}
 
 // Cambio de tema claro/oscuro
 const toggleThemeBtn = document.getElementById('toggle-theme-btn');
 toggleThemeBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
+
+// Reproducir música
+const playButton = document.getElementById('playMusic');
+const music = document.getElementById('bgMusic');
+
+playButton.addEventListener('click', () => {
+  if (music.paused) {
+    music.play();
+    playButton.textContent = '⏸️ Pausar música';
+  } else {
+    music.pause();
+    playButton.textContent = '🎵 Reproducir música';
+  }
+});
+
+
